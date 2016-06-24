1485b9e06d988df6b858667ea3bbf2bfae0309c9
TaikiKosugi & Ben Behrman <pair+TaikiKosugi.bbehrman10@devbootcamp.com>
2016-06-24 11:34:11 -0400

    adds deck working, just  needs to have flashcard creation line

---
+++ deck.rb
@@ -0,0 +1,32 @@
+class Deck
+  require 'pry'
+
+  def initialize(text_file)
+    @file = text_file
+    @flashcards = []
+  end
+
+  def read_file
+    File.open(@file, "r") do |file|
+      input_hash = {}
+      counter = 1
+      file.each_line do |line|
+        if counter % 3 != 0
+          if counter % 3 == 1
+            input_hash[:question] = line
+          else
+            input_hash[:answer] = line
+             #flashcard = FlashCard.new(input_hash)
+            p input_hash
+            input_hash = {}
+
+          end
+        end
+        counter += 1
+      end
+    end
+  end
+end
+
+test = Deck.new("raccoon_flashcard_data.txt")
+test.read_file
