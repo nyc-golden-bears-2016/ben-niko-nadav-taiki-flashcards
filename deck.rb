becd1f5473f5c8382937410fdc6b58691032d1f6
TaikiKosugi & Ben Behrman <pair+TaikiKosugi.bbehrman10@devbootcamp.com>
2016-06-24 16:25:20 -0400

    refactors everything, makes it all work with the correct model thing pan talked about mvc? i think now im rambling in this commit message

--- deck.rb
+++ deck.rb
@@ -2,12 +2,41 @@ class Deck
 
   attr_reader :flashcards
 
-  def initialize
+  def initialize(text_file)
+    @file = text_file
     @flashcards = []
+
   end
 
   def shuffle
     @flashcards.shuffle!
+  end
+
+  def build_deck
+    if File.exist?(@file)
+      if File.readable?(@file)
+        File.open(@file, "r") do |file|
+          input_hash = {}
+          counter = 1
+          file.each_line do |line|
+            if counter % 3 != 0
+              if counter % 3 == 1
+                input_hash[:question] = line.chomp
+              else
+                input_hash[:answer] = line.chomp
+                @flashcards << FlashCard.new(input_hash)
+                input_hash = {}
+              end
+            end
+            counter += 1
+          end
+        end
+      else
+        "file isn't readable"
+      end
+    else
+      "file doesn't exist"
+    end
   end
 end
 
--- view.rb
+++ view.rb
@@ -1,23 +1,41 @@
-require_relative 'controller'
 class View
-  def initialize(file)
-    @file = file
+
+  def display_question(question)
+    puts "#{question}"
+    gets.chomp
   end
 
-  def run
-    @control = Controller.New(@file)
-    # correct_answer = 0
-    @control.deck.each do |question|
-      puts @control.ask_question
-      if control.check_answer(answer = gets.chomp)
-        # correct_answer += 1
-        puts "Correct!"
-      else
-        puts "Incorrect!"
-      end
-    end
-    puts "You solved, #{controller.correct_answer_counter} many questions correctly"
+  def display_correct
+    puts "Correct!!!"
+  end
+
+  def display_incorrect
+    puts "Incorrect!!!"
+  end
+
+  def display_number_of_correct(correct, total)
+    puts "You got #{correct} of #{total} correct!!! "
+    puts "Press return to continue"
+  end
+
+  def display_menu
+    clearscreen
+    puts "Welcome to the QUIZ-A-TRON 5000"
+    puts "================================"
+    puts "Please select a set of questions:"
+    puts "--------------------------------"
+    puts "1. Nighthawks"
+    puts "2. Otter"
+    puts "3. Raccoon\n\n"
+    puts "To exit, type 'exit'\n\n\n"
+  end
+
+  def clearscreen
+    puts "\e[H\e[2J"
+  end
+
+  def display_choice(quiz)
+    "#{quiz} Selected."
   end
 end
-
 
