c87755952ddfcbd19b7c64a61a7b0a29acf835fa
TaikiKosugi & Ben Behrman <pair+TaikiKosugi.bbehrman10@devbootcamp.com>
2016-06-24 12:08:26 -0400

    fully integrates deck class with flashcards class

--- deck.rb
+++ deck.rb
@@ -1,25 +1,26 @@
+require_relative 'flashcards'
 class Deck
-  require 'pry'
 
   attr_reader :flashcards
 
   def initialize(text_file)
     @file = text_file
     @flashcards = []
+    read_file
   end
 
   def read_file
     if File.exist?(@file)
-      if File.readable(@file)
+      if File.readable?(@file)
         File.open(@file, "r") do |file|
           input_hash = {}
           counter = 1
           file.each_line do |line|
             if counter % 3 != 0
               if counter % 3 == 1
-                input_hash[:question] = line
+                input_hash[:question] = line.chomp
               else
-                input_hash[:answer] = line
+                input_hash[:answer] = line.chomp
                 @flashcards << FlashCard.new(input_hash)
                 input_hash = {}
               end
@@ -40,6 +41,5 @@ class Deck
     @flashcards.shuffle!
   end
 end
-
 
 
